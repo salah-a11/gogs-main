bce374caad31bccd398c1d2aed79ac2cee949c03
youcef achira <115021298+salah-a11@users.noreply.github.com>
2022-12-05 02:10:41 +0100

    Add files via upload

--- embed.go
+++ embed.go
@@ -2,26 +2,11 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-package conf
+package public
 
 import (
 	"embed"
 )
 
-//go:embed app.ini **/*
+//go:embed assets/* css/* img/* js/* plugins/*
 var Files embed.FS
-
-// FileNames returns a list of filenames exists in the given direction within
-// Files. The list includes names of subdirectories.
-func FileNames(dir string) ([]string, error) {
-	entries, err := Files.ReadDir(dir)
-	if err != nil {
-		return nil, err
-	}
-
-	fileNames := make([]string, 0, len(entries))
-	for _, entry := range entries {
-		fileNames = append(fileNames, entry.Name())
-	}
-	return fileNames, nil
-}
